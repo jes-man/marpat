4b085229015bf5ed53da30445c256236f51a92cf
Lui de la Parra <Lui@mutesymphony.com>
2018-10-26 19:48:04 -0700

    Adds Connect Testing
    This commit adds additional testing to the connect function. This commit also revises mocha tests to automatically exit.
    
    Signed-off-by: Lui de la Parra <Lui@mutesymphony.com>

--- test/connect.test.js
+++ test/connect.test.js
@@ -10,6 +10,16 @@ const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 
 describe('Connect Capability', () => {
+  it('should connect to an nedb connection', function() {
+    return expect(connect('nedb://memory'))
+      .to.eventually.be.an('object')
+      .with.all.keys('_collections', '_options', '_path', '_url');
+  });
+  it('should connect to a mongodb connection', function() {
+    return expect(connect('mongodb://127.0.0.1:27017/marpat'))
+      .to.eventually.be.an('object')
+      .with.all.keys('_mongo', '_url');
+  });
   it('should reject an unrecognized connection', function() {
     return expect(
       connect('FIREBASE://memory').catch(error => error)
