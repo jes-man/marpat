6787eb5ec8043f236c1c44a629288d2f3f6cc8d6
Lui de la Parra <Lui@mutesymphony.com>
2018-10-26 20:22:19 -0700

    Revises Support Type Testing
    This commit revises the supported type validation to properly throw errors for null or undefined values.
    
    Signed-off-by: Lui de la Parra <Lui@mutesymphony.com>

--- test/validations.test.js
+++ test/validations.test.js
@@ -1,11 +1,9 @@
 'use strict';
 
-/* global describe before beforeEach afterEach it */
+/* global describe it */
 
 const { expect } = require('chai');
-const { connect } = require('../index');
-const { isType } = require('../lib/validate');
-const { EmbeddedData } = require('./data');
+const { isType, isSupportedType } = require('../lib/validate');
 
 describe('Validation Utility Tests', () => {
   it('should throw an error if the type is unsupported', () =>
@@ -14,7 +12,20 @@ describe('Validation Utility Tests', () => {
   describe('Type Detection', () => {
     let array = ['one', 'two', 'three'];
     it('should detect Arrays', () => expect(isType(array, Array)).to.be.true);
+    it('should reject undefined', () =>
+      expect(() => isType(undefined, undefined)).to.throw);
   });
 
-
+  describe('Supported Type Detection', () => {
+    it('should support strings', () =>
+      expect(isSupportedType(String)).to.be.true);
+    it('should support numbers', () =>
+      expect(isSupportedType(Number)).to.be.true);
+    it('should support object', () =>
+      expect(isSupportedType(Object)).to.be.true);
+    it('should support Arrays', () =>
+      expect(isSupportedType(Array)).to.be.true);
+    it('should support booleans', () =>
+      expect(isSupportedType(Boolean)).to.be.true);
+  });
 });
